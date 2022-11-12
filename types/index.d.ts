df9f31584b469364343948b9590d7359998dd7b4
nick-gabe <nickygamer023@gmail.com>
2022-11-12 12:43:20 -0300

    starts refactoring types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,7 @@
-type availableLanguages = 'en' | 'pt'
+declare type AvailableLanguages = 'en' | 'pt'
 
-/// <reference path="main.d.ts" />
-/// <reference path="utils.d.ts" />
+declare type DateStringifier = (date: Date, format: string, language: AvailableLanguages) => string
+
+interface ErrorConstructor {
+  captureStackTrace(thisArg: any, func: any): void
+}
